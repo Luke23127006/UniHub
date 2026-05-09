48471db9f706232136f30e77e208f97e7f2c84b4
Tran Nguyen Khai Luan <tnkluan23@clc.fitus.edu.vn>
2026-05-09 16:13:13 +0700

    fix: loadtest opion (12000 students instead of 12000 requests

--- backend/src/tests/loadtest.js
+++ backend/src/tests/loadtest.js
@@ -1,26 +1,23 @@
 import http from 'k6/http';
 import { check } from 'k6';
 
-// -----------------------------------------------------------
-// K6 LOAD TEST CONFIGURATION
-// -----------------------------------------------------------
 export const options = {
   scenarios: {
     registration_spike: {
       executor: 'ramping-arrival-rate',
       startRate: 0,
-      timeUnit: '1s',       // Calculate based on rate (requests per second)
-      preAllocatedVUs: 100, // Pre-allocate 100 virtual users
-      maxVUs: 1000,         // Allow K6 to scale up to 1000 virtual users if needed
+      timeUnit: '1s', 
+      preAllocatedVUs: 100,
+      maxVUs: 2000,
       stages: [
         // --- PHASE 1: FIRST 3 MINUTES (Spike 40 requests/s) ---
-        { duration: '10s', target: 40 },   // Ramp up quickly to 40 requests per second in 10s
-        { duration: '2m40s', target: 40 }, // Maintain high intensity of 40 req/s (60% of students)
-        { duration: '10s', target: 12 },   // Reduce to normal level
+        { duration: '10s', target: 200 },
+        { duration: '2m40s', target: 200 },
+        { duration: '10s', target: 60 },
 
         // --- PHASE 2: NEXT 7 MINUTES (Stable ~12 requests/s) ---
-        { duration: '6m50s', target: 12 }, // Maintain stable rate of 12 req/s (40% of students)
-        { duration: '10s', target: 0 },    // Lower gradually to 0 and end
+        { duration: '6m50s', target: 60 },
+        { duration: '10s', target: 0 },
       ],
     },
   },
@@ -28,16 +25,13 @@ export const options = {
   // Because we have Rate Limit, the server returning 429 (Too Many Requests) is a SUCCESSFUL protection of the system, not an error.
 };
 
-// -----------------------------------------------------------
-// TEST EXECUTION FUNCTION (Runs for each iteration)
-// -----------------------------------------------------------
 export default function () {
   // Replace with your actual local endpoint. Here we use workshop ID 1.
   const url = __ENV.K6_API_URL || 'http://localhost:3000/api/v1/workshops/1/register';
 
-  // Generate a random user ID between 1 and 100,000
+  // Generate a random user ID between 1 and 12,000
   // This prevents unique constraint violations in the DB when the worker processes the queue
-  const randomUserId = Math.floor(Math.random() * 100000) + 1;
+  const randomUserId = Math.floor(Math.random() * 12000) + 1;
 
   // The controller extracts data from route params and headers, 
   // so the payload body can just be empty.
