2a5245c1ff975f14b38c15e69f9cec75e8efa34d
Tran Nguyen Khai Luan <tnkluan23@clc.fitus.edu.vn>
2026-05-09 15:29:58 +0700

    add rate limit

--- backend/src/middlewares/rateLimitMiddleware.js
+++ backend/src/middlewares/rateLimitMiddleware.js
@@ -1,3 +1,13 @@
-const { registrationLimiter } = require('./rateLimiter.middleware');
+const rateLimit = require('express-rate-limit');
 
-module.exports = { workshopRegistrationLimiter: registrationLimiter };
+const workshopRegistrationLimiter = rateLimit({
+  windowMs: 10 * 1000, // Time window: 10 seconds
+  max: 3, // Limit: Maximum 3 requests / 10 seconds per IP
+  message: {
+    message: 'The system is busy processing, please do not spam. Please try again in a few seconds.'
+  },
+  standardHeaders: true, // Return RateLimit-* headers
+  legacyHeaders: false, // Disable X-RateLimit-* headers
+});
+
+module.exports = { workshopRegistrationLimiter };
